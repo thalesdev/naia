405778313c0eaeb567fb06c10886974bc661d1ca
Thales de Oliveira <thales@caju.com.br>
2022-11-20 22:08:04 -0300

    chore: fix env variables in lambda functions

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    AMADEUS_CLIENT_ID: process.env.AMADEUS_CLIENT_ID,
+    AMADEUS_CLIENT_SECRET: process.env.AMADEUS_CLIENT_SECRET,
+  }
 }
 module.exports = nextConfig
